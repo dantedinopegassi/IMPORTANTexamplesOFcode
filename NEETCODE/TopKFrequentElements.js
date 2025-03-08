4b7baeef6c2fa5d98777a3fc82eed3019d393a0d
dinopegassi <nmbost@gmail.com>
2025-03-09 02:10:36 +0930

    FIX: TopKFrequentElements

--- NEETCODE/TopKFrequentElements.js
+++ NEETCODE/TopKFrequentElements.js
@@ -5,24 +5,36 @@ class Solution {
      * @return {number[]}
      */
     topKFrequent(nums, k) {
+        // a frequency map
         let freqMap = {};
         for (let i = 0 ; i < nums.length ; i++){
             freqMap[nums[i]] = (freqMap[nums[i]]) ? freqMap[nums[i]] + 1 : 1;
         }
 
+        // this is bucket sort
         let array = [];
         for (let key in freqMap) {
-            array[freqMap[key]] = key;
+            (!array[freqMap[key]]) ? array[freqMap[key]] = [] : null;
+            array[freqMap[key]].push(key);
         }
+
+        /**
+         * it might seem like we need to sort each bucket, but
+         * the problem explicitly states that we can return the
+         * output in any order, so no sorting...
+         */ 
 
         let counter = 0;
         let solution = [];
-        for (let i = array.length - 1 ; i > -1  ; i--){
-            if (k>counter){
-                solution[counter] = array[i];
-                counter++;
-            } else break;
+        for (let i = array.length - 1 ; i > -1 && solution.length < k ; i--){
+            if (array[i]){
+                for(let value of array[i]){
+                    solution[counter] = value;
+                    counter++;
+                }
+            }
         }
+        
 
         return solution;
     }
